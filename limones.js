1d5d7de8be5790110f7e622c5733b636a6431684
edwinArellano <edwinarellano77@gmail.com>
2026-04-12 23:00:00 -0400

    FUNCIONES SUELO Y PERSONAJE

--- limones.js
+++ limones.js
@@ -1,2 +1,24 @@
 let canvas=document.getElementById("areaJuego");
 let ctx=canvas.getContext("2d");
+
+//CONSTANTES
+const ALTURA_SUELO=20;
+const ALTURA_PERSONAJE=60;
+const ANCHO_PERSONAJE=40;
+// INICIA JUEGO
+function iniciar(){
+    dibujarSuelo();
+    dibujarPersonaje();
+}
+
+//DIBUJAR SUELO
+function dibujarSuelo(){
+    ctx.fillStyle="blue";
+    ctx.fillRect(0,canvas.height-ALTURA_SUELO,canvas.width,ALTURA_SUELO);
+}
+
+// DIBUJAR PERSONAJE
+function dibujarPersonaje(){
+   ctx.fillStyle="yellow";
+    ctx.fillRect(canvas.width/2,canvas.height-(ALTURA_SUELO+ALTURA_PERSONAJE),40,ALTURA_PERSONAJE);
+}
